e430ff51c1cde8f6dcff34278f317494b5a6a87b
Fernando Garcia Diaz <fmgarciadiaz78@gmail.com>
2020-11-19 07:20:14 +0000

    version 0.1.6

--- inst/doc/Tutorial.R
+++ inst/doc/Tutorial.R
@@ -14,12 +14,14 @@ Series <- Search_online("Tipo de cambio")
 tail(Series,10)
 
 ## ----descarga-----------------------------------------------------------------
-TCN <- Get("174.1_T_DE_CATES_0_0_32", start_date = "2010")
+TCN <- Get("174.1_T_DE_CATES_0_0_32", start_date = "2010", timeout = 15)
 
 ## ----forecast-----------------------------------------------------------------
+if (length(TCN) > 0)
 TCN <- Forecast(TCN, N = 12 , confidence = c(80))
 
 ## ----graficar, fig.align= "center", fig.height= 3, fig.width= 7---------------
+if (length(TCN) > 0){
 tail(TCN, 10)
-plot(TCN , main = "Tipo de Cambio Nominal ($/USD)")
+plot(TCN , main = "Tipo de Cambio Nominal ($/USD)")}
 
